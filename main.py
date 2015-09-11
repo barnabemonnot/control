939c8969a4c395205f0946f9e41c859e36c1568c
Barnabé Monnot <barnabe.monnot@yahoo.fr>
2015-09-11 21:18:33 +0200

    Added synthetic graphs

--- main.py
+++ main.py
@@ -1,21 +1,80 @@
 import weightanalysis as wa
 import argparse
+import numpy as np
 import matplotlib
 matplotlib.use('tkAgg')
 import matplotlib.pyplot as plt
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--n', '--network', type=str, default='', help='network', metavar='n')
 args = parser.parse_args()
 
-G = wa.load_network(args.n)
-step = 20
-#(deg, in_deg, out_deg) = wa.degree_distribution(G)
-#print "Will plot degree distribution"
-#wa.plot_deg_distrib(G)
+# filename = wa.filename(args.n)
+# print filename
+# G = wa.load_network(args.n)
+filename="erinv"
+G = wa.random_erdos()
+avg = wa.avg_reducer(G, 1, filename=filename)
+r_avg = wa.avg_reducer(G, 1, rand=True, filename=filename+"-rand")
+# desc = {
+#     "intl": "International airport connections",
+#     "us": "US airport connections",
+#     "celegans": "Neural network of \\textit{C.Elegans} bacteria",
+#     "Chesapeake": "Foodwebs",
+#     "ChesLower": "Foodwebs",
+#     "ChesMiddle": "Foodwebs",
+#     "ChesUpper": "Foodwebs",
+#     "CrystalC": "Foodwebs",
+#     "CrystalD": "Foodwebs",
+#     "Everglades": "Foodwebs",
+#     "Florida": "Foodwebs",
+#     "Maspalomas": "Foodwebs",
+#     "Michigan": "Foodwebs",
+#     "Mondego": "Foodwebs",
+#     "Narragan": "Foodwebs",
+#     "Rhode": "Foodwebs",
+#     "StMarks": "Foodwebs",
+#     "days1-12": "Mail logs (all days)",
+#     "days1-4": "Mail logs",
+#     "days5-8": "Mail logs",
+#     "days9-12": "Mail logs",
+#     "mention_network": "Twitter network of mentions",
+#     "one_mode_char": "ucirvine",
+#     "one_mode_message": "ucirvine"
+# }
+#
+# # Name / Description / #Node / #Edges (/ gamma) / rho /
+# print "\hline"
+# print "%s & %s & %d & %d & %f & %f \\\\" % (filename, desc[filename], len(G), G.size(), wa.get_exponent(G), wa.correlations(G))
+# step = 1
+
+#print 'Avg_reducer weight'
+#avg = wa.avg_reducer(G, step, runs=20, filename=filename)
+#print 'Avg_reducer random'
+#r_avg = wa.avg_reducer(G, step, rand=True, runs=20, filename=filename+"-rand")
+
+# controls = np.loadtxt("controls/"+filename+".txt")
+# profile = controls[:,range(1,4)]
+# rcontrols = np.loadtxt("controls/"+filename+"-rand.txt")
+# rprofile = rcontrols[:,range(1,4)]
+#wa.draw_ternary_plot(profile, rprofile, filename)
+wa.plot_min_number_controls(avg[:,0], r_avg[:,0], filename)
+#wa.gg_print_control(controls)
+#(avg_in, avg_out, avg, total_in, total_out, total) = wa.avg_weights(G)
+#for nidx in G.nodes_iter_():
+#	if G.out_degree_(nidx) == 0:
+#		#print "I am a sink with %d neighbors" % G.in_degree_(nidx)
+#		for eidx, w in G.edges_iter_(nidx, weight=True):
+#			#print "I have weights %f" % w
 #print "Compute weight cuts"
 #(con, diff, weight, node, controlled) = wa.reducer(G, step, asc=False)
 #print "Compute random cuts"
 #(rcon, rdiff, rweight, rnode, rcontrolled) = wa.reducer(G, step, rand=True, asc=False)
+
+#(deg, in_deg, out_deg) = wa.degree_distribution(G)
+#print "Will plot degree distribution"
+#wa.plot_deg_distrib(G)
+#wa.plot_link_weights(G, filename)
 #print "Print graphs"
 #filename = wa.filename(args.n)
 #wa.print_graphs(con, rcon, diff, weight, node, rnode, filename)
@@ -24,9 +83,4 @@ step = 20
 #wa.show_weights(G)
 #(corr_deg, corr_in, corr_out) = wa.correlations(G)
 #print "Network %s, correlations deg = %f, in = %f, out = %f" % (args.n, corr_deg, corr_in, corr_out)
-print 'Avg_reducer weight'
-filename = wa.filename(args.n)
-avg = wa.avg_reducer(G, step, runs=20, filename=filename)
-print 'Avg_reducer random'
-r_avg = wa.avg_reducer(G, step, rand=True, runs=20, filename=filename+"rand")
-wa.print_control_graphs(avg, r_avg, wa.filename(args.n)+'avg')
+#wa.print_control_graphs(avg, r_avg, wa.filename(args.n)+'avg')
--- weightanalysis.py
+++ weightanalysis.py
@@ -6,28 +6,120 @@ import zen
 import random
 import os
 import time
-import seaborn as sns
+import pandas as pd
+import ggplot as gg
+import powerlaw
+import ternary
 
 def load_network(networkname):
 	G = zen.io.edgelist.read(networkname, directed=True, weighted=True)
 	return G
 
+def random_barabasi():
+    G = zen.generating.barabasi_albert(100, 10, directed=True)
+    for eidx in G.edges_iter_():
+        (src, tgt) = G.endpoints_(eidx)
+        #G.set_weight_(eidx, random.randint(1,100)*(G.degree_(src)+G.degree_(tgt)))
+        G.set_weight_(eidx, random.randint(1,100))
+    return G
+    
+def random_erdos():
+    G = zen.generating.erdos_renyi(100, 0.02, directed=True)
+    for eidx in G.edges_iter_():
+        (src, tgt) = G.endpoints_(eidx)
+        G.set_weight_(eidx, random.randint(1,100)/(G.degree_(src)+G.degree_(tgt)))
+        #G.set_weight_(eidx, random.randint(1,100))
+    return G
+
+def filename(string):
+	return string.split("/")[-1].split('.')[0]
+
 def degree_distribution(G):
-	in_degrees = [G.in_degree_(nidx) for nidx in G.nodes_iter_()]
-	out_degrees = [G.out_degree_(nidx) for nidx in G.nodes_iter_()]
-	degrees = [G.degree_(nidx) for nidx in G.nodes_iter_()]
+	in_degrees = np.array([G.in_degree_(nidx) for nidx in G.nodes_iter_()])
+	out_degrees = np.array([G.out_degree_(nidx) for nidx in G.nodes_iter_()])
+	degrees = np.array([G.degree_(nidx) for nidx in G.nodes_iter_()])
 	return (in_degrees, out_degrees, degrees)
 
 def plot_deg_distrib(G):
 	(in_deg, out_deg, deg) = degree_distribution(G)
-	plt.figure(1)
-	plt.subplot(131)
-	plt.hist(in_deg)
-	plt.subplot(132)
-	plt.hist(out_deg)
-	plt.subplot(133)
-	plt.hist(deg)
-	plt.show()
+	in_deg_series = pd.Series(in_deg)
+	out_deg_series = pd.Series(out_deg)
+	in_out = { 'in_deg': in_deg_series, 'out_deg': out_deg_series }
+	df = pd.DataFrame(in_out)
+	df = pd.melt(df)
+	p = gg.ggplot(gg.aes(x='value', color='variable', fill='variable'), data=df2) + gg.geom_histogram(alpha=0.6, binwidth=1)
+	print p
+
+def avg_weights(G):
+    (indeg, outdeg, deg) = degree_distribution(G)
+    maxin = max(indeg)+1
+    maxout = max(outdeg)+1
+    total_max = max(deg)+1
+    number_in = np.zeros((maxin, maxout)) # (k, l) is the number of in-neighbors for nodes of type (k, l)
+    number_out = np.zeros((maxin, maxout)) # (k, l) is the number of out-neighbors for nodes of type (k, l)
+    total_number = np.zeros(total_max) # (k) is the number of neighbors for nodes of degree k
+    weight_in = np.zeros((maxin, maxout)) # (k, l) is the total weight of in-edges for nodes of type (k, l)
+    weight_out = np.zeros((maxin, maxout)) # (k, l) is the total weight of out-edges for nodes of type (k, l)
+    total_weight = np.zeros(total_max) # (k) is the total weight of edges for nodes of degree k
+    avg_in = np.zeros((maxin, maxout)) # (k, l) is the average weight of in-edges for nodes of type (k, l)
+    avg_out = np.zeros((maxin, maxout)) # (k, l) is the average weight of out-edges for nodes of type (k, l)
+    avg = np.zeros((maxin, maxout)) # (k, l) is the average weight of edges for nodes of type (k, l)
+    total_avg_in = np.zeros(maxin) # (k) is the average weight of edges for nodes of in-degree k
+    total_avg_out = np.zeros(maxout) # (k) is the average weight of edges for nodes of out-degree k
+    total_avg = np.zeros(total_max) # (k) is the average weight of edges for nodes of degree k
+
+    for nidx in G.nodes_iter_():
+    	node_out_deg = G.out_degree_(nidx)
+    	node_in_deg = G.in_degree_(nidx)
+    	node_deg = G.degree_(nidx)
+    	number_in[node_in_deg, node_out_deg] += node_in_deg
+    	number_out[node_in_deg, node_out_deg] += node_out_deg
+    	total_number[node_deg] += node_deg
+    	for eidx, w in G.in_edges_iter_(nidx, weight=True): # edges for which nidx is a target
+    		weight_in[node_in_deg, node_out_deg] += w
+    		total_weight[node_deg] += w
+            
+    	for eidx, w in G.out_edges_iter_(nidx, weight=True):
+    		weight_out[node_in_deg, node_out_deg] += w
+    		total_weight[node_deg] += w
+            
+    for k in range(0, maxin):
+    	for l in range(0, maxout):
+    		if number_in[k, l] > 0:
+    			avg_in[k,l] = weight_in[k,l]/number_in[k,l]
+    		if number_out[k, l] > 0:
+    			avg_out[k,l] = weight_out[k,l]/number_out[k,l]
+    		if number_in[k,l] > 0 or number_out[k,l] > 0:
+    			avg[k,l] = (weight_in[k,l]+weight_out[k,l])/(number_in[k,l]+number_out[k,l])
+
+    for k in range(0, total_max):
+    	if total_number[k] > 0:
+    		total_avg[k] = total_weight[k] / total_number[k]       
+
+    # num = np.multiply(weight_in, number_in)
+#     num_in = np.sum(num, axis=1)
+#     den_in = np.sum(total_number_in_out, axis=1)
+#     num_out = np.sum(num, axis=0)
+#     den_out = np.sum(total_number_in_out, axis=0)
+#     for k in range(0, maxin):
+#         if den_in[k] > 0:
+#             total_avg_in[k] = num_in[k]/den_in[k]
+#     for k in range(0, maxout):
+#         if den_out[k] > 0:
+#             total_avg_out[k] = num_out[k]/den_out[k]
+    
+	#source_out = avg_in[:,0]
+	#df = pd.DataFrame({ 'idx': pd.Series(range(0, maxin)), 'source_out': pd.Series(source_out) })
+	#p = gg.ggplot(gg.aes(x='idx', y='source_out'), data=df) + gg.geom_line()
+	#print p
+#     fig = plt.figure()
+#     plt.imshow(avg_in/np.max(avg_in), cmap='Blues', interpolation='nearest', aspect='auto')
+#     plt.show()
+
+    # fig = plt.figure()
+    # plt.bar(range(0, total_max), total_avg)
+    # plt.show()
+    return (avg_in, avg_out, avg, total_avg_in, total_avg_out, total_avg)
 
 def get_controlled_nodes(G):
 	matched_edges = zen.maximum_matching_(G)
@@ -95,7 +187,7 @@ def avg_reducer(H, step, runs=100, rand=False, asc=True, filename=""):
 	for i in range(0, runs):
 		print 'Run %d' % i
 		[controls, diff_controls, weight_cuts, node_removed, controlled_nodes] = reducer(H, step, rand, asc)
-		avg_controls = avg_controls+controls/100
+		avg_controls = avg_controls+controls/runs
 	if filename != "":
 		np.savetxt("controls/"+filename+".txt", avg_controls)
 	return avg_controls
@@ -164,6 +256,14 @@ def reducer(H, step, rand=False, asc=True):
 			diff_controls[i-1] = m-controls[i-1][0]
 	print 'Ran in %f seconds' % (time.time()-total_running_time)
 	return controls, diff_controls, weight_cuts, node_removed, controlled_nodes
+
+def gg_print_control(controls):
+	N = len(controls[:,0])
+	ind = np.arange(N)
+	width = 1
+	df = pd.DataFrame({ "ind": ind, "src": pd.Series(controls[:,1]), "ext": pd.Series(controls[:,2]), "int": pd.Series(controls[:,3])})
+	p = gg.ggplot(gg.aes(x='ind', y='src'), data=df) + gg.geom_line()
+	print p
 
 def print_control_graphs(controls, rcontrols, filename):
 	N = len(controls[:,0])
@@ -268,67 +368,65 @@ def print_graphs(controls, rcontrols, diff_controls, weight_cuts, node_removed, 
 	fig.savefig('graphs/'+filename+'-controls.png')
 	plt.show()
 
-def avg_degrees_(G, nidx):
-	total = 0.0
-	total_in = 0.0
-	total_out = 0.0
-	avg = 0.0
-	avg_in = 0.0
-	avg_out = 0.0
-	for eidx, w in G.edges_iter_(nidx, weight=True):
-		total += w
-	for eidx, w in G.in_edges_iter_(nidx, weight=True):
-		total_in += w
-	for eidx, w in G.out_edges_iter_(nidx, weight=True):
-		total_out += w
-	if G.degree_(nidx) > 0:
-		avg = total/G.degree_(nidx)
-	if G.in_degree_(nidx) > 0:
-		avg_in = total_in/G.in_degree_(nidx)
-	if G.out_degree_(nidx) > 0:
-		avg_out = total_out/G.out_degree_(nidx)
-	return avg, avg_in, avg_out
-
-def weight_means(G):
-	total = np.zeros(len(G))
-	total_in = np.zeros(len(G))
-	total_out = np.zeros(len(G))
-	for nidx in G.nodes_iter_():
-		(avg, avg_in, avg_out) = avg_degrees_(G, nidx)
-		total[nidx] = avg
-		total_in[nidx] = avg_in
-		total_out[nidx] = avg_out
-	return total, total_in, total_out
-
-def scatter_plot(x, y):
-	fig = plt.figure()
-	plt.scatter(x, y)
-	plt.show()
-
-def show_weights(G):
-	(total, total_in, total_out) = weight_means(G)
-	(deg, in_deg, out_deg) = degree_distribution(G)
-	fig = plt.figure()
-	plt.subplot(3,1,1)
-	plt.scatter(deg, total)
-	plt.subplot(3,1,2)
-	plt.scatter(in_deg, total_in)
-	plt.subplot(3,1,3)
-	plt.scatter(out_deg, total_out)
-	plt.show()	
+def remove_values_from_list(the_list, val):
+   return [i for i in range(0, len(the_list)) if the_list[i] != val]
 
 def correlations(G):
-	(total, total_in, total_out) = weight_means(G)
-	(deg, in_deg, out_deg) = degree_distribution(G)
-	return np.corrcoef(total, deg)[0,1], np.corrcoef(total_in, in_deg)[0,1], np.corrcoef(total_out, out_deg)[0,1]
+    (avg_in, avg_out, avg, total_avg_in, total_avg_out, total_avg) = avg_weights(G)
+    (in_deg, out_deg, deg) = degree_distribution(G)
+    nz = total_avg[np.nonzero(total_avg)]
+    r = np.array(range(0, len(total_avg)))
+    r = r[np.nonzero(total_avg)]
+    return np.corrcoef(nz, r)[0,1]
 		
-def tag_nodes(G):
-	controlled_nodes_slice, matching = get_controlled_nodes(G)
-	(source, external, internal), types = control_profile_(G, controlled_nodes_slice, matching)
-	return types
-
-def filename(string):
-	return string.split("/")[-1].split('.')[0]
-
-
+def plot_link_weights(G, filename):
+	print "Filename = %s" % filename
+	w = np.array([w for u,v,w in G.edges_iter(weight=True)])
+	fit = powerlaw.Fit(w)
+	R, p = fit.distribution_compare('power_law', 'exponential', normalized_ratio=True)
+	title = "Exponent: %f" % fit.power_law.alpha
+	print "(R, p) = (%f, %f)" % (R, p)
+	df = pd.DataFrame({ 'weight': pd.Series(w) })
+	p = gg.ggplot(gg.aes(x='weight'), data=df) + gg.geom_histogram(fill='blue', alpha=0.6) + gg.scale_y_log10() + gg.ggtitle(title)
+	gg.ggsave(filename="graphs/weight_distrib/"+filename+"-weight_distrib.png", plot=p)
 	
+def get_exponent(G):
+    w = np.array([w for u,v,w in G.edges_iter(weight=True)])
+    fit = powerlaw.Fit(w)
+    R, p = fit.distribution_compare('power_law', 'exponential', normalized_ratio=True)
+    return fit.power_law.alpha
+
+def normalize_weights(G):
+	w = np.array([w for u,v,w in G.edges_iter(weight=True)])
+	max_weight = max(w)
+	min_weight = min(w)
+	for eidx in G.edges_iter_():
+		G.set_weight_(eidx, (G.weight_(eidx)-min_weight)/(max_weight-min_weight)+1)
+	return G
+
+def draw_ternary_plot(controls, rcontrols, filename):
+	figure, tax = ternary.figure(scale=1.0)
+	tax.boundary(color='black')
+	tax.plot(controls, linewidth=2.0, label="Weight cuts", color="blue")
+	tax.plot(rcontrols, linewidth=2.0, label="Random cuts", color="green")
+	tax.left_axis_label("No source control")
+	tax.right_axis_label("No internal dilation control")
+	tax.bottom_axis_label("No external dilation control")
+	tax.legend()
+	#tax.show()
+	figure.savefig('graphs/ternary/'+filename+'-ternary.png')
+
+def plot_min_number_controls(controls, rcontrols, filename):
+	fig = plt.figure()
+	plt.plot(range(0, len(controls)), controls, 'b-', label='Weight cuts', linewidth=2.0)
+	plt.plot(range(0, len(rcontrols)), rcontrols, 'g-', label='Random cuts', linewidth=2.0)
+	plt.legend(loc=2)
+	plt.title(filename)
+	plt.ylabel('Number of controls')
+	plt.xlabel('Slices')
+#	plt.show()
+	fig.savefig('graphs/number_controls/'+filename+'-controls.png')
+
+
+
+
